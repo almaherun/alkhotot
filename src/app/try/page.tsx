82959500ace5e760325a593a36a17dfe3f6bc704
Almaheron Academy <almaheruon@gmail.com>
2025-08-22 08:24:10 +0000

    Wrap TryPage in Suspense for prerendering

--- src/app/try/page.tsx
+++ src/app/try/page.tsx
@@ -1,10 +1,13 @@
 import AppLayout from "@/components/app-layout";
 import { TryPage } from "@/components/try-page";
+import { Suspense } from "react";
 
 export default function TryRoute() {
   return (
     <AppLayout>
-      <TryPage />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TryPage />
+      </Suspense>
     </AppLayout>
   );
 }
